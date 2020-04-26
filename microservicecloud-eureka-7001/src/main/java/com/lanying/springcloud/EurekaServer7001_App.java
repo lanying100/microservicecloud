4c7e6bb8b6358958462105f1e3aec165906ae6dc
lanying <761562013@qq.com>
2020-04-26 11:51:48 +0800

    将部门服务Provider注册进Eureka服务中心

--- microservicecloud-eureka-7001/src/main/java/com/lanying/springcloud/EurekaServer7001_App.java
+++ microservicecloud-eureka-7001/src/main/java/com/lanying/springcloud/EurekaServer7001_App.java
@@ -1,4 +1,4 @@
-package com.lanying;
+package com.lanying.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- microservicecloud-provider-dept-8001/src/main/java/com/lanying/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/lanying/springcloud/DeptProvider8001_App.java
@@ -2,8 +2,10 @@ package com.lanying.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient // 3.本服务启动后会自动注册进Eureka服务中
 public class DeptProvider8001_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider8001_App.class, args);
